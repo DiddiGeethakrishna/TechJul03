8731ff58b68b520fe3f3dce61d6ecad9cd48fc67
DiddiGeethakrishna <70500217+DiddiGeethakrishna@users.noreply.github.com>
2022-08-05 09:19:54 +0530

    Add files via upload

---
+++ Microservices Assignments/Class Assignment/PassengerManagement/Models/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassengerManagement.Models
+{
+    public class Passenger 
+    {
+        public int PassengerId { get; set; }
+        public string PassengerName { get; set; }
+        public string PickupLocation { get; set; }
+
+    }
+}
